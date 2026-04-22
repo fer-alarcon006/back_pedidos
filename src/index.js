2183a661dea2c89dbf9544918561e87720c02728
fer-alarcon006 <fer_alice15@outlook.es>
2026-04-21 22:16:45 -0600

    noveno  comentario

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import { app } from "./app.js"; // si lo tienes separado
-import { initBaseDeDatos } from "./config/db.js";
+import { app } from "./app.js";
 
 async function iniciarServidor() {
   try {
     console.log("Iniciando servidor...");
 
-    await initBaseDeDatos();
-
-    const PORT = process.env.PORT;
-
-    if (!PORT) {
-      throw new Error("PORT no definido por Render");
-    }
+    const PORT = process.env.PORT || 10000;
 
     app.listen(PORT, "0.0.0.0", () => {
       console.log(`Servidor corriendo en puerto ${PORT}`);
@@ -19,10 +12,9 @@ async function iniciarServidor() {
 
   } catch (error) {
     console.error("ERROR CRÍTICO:", error);
-    process.exit(1);
   }
 }
 
 iniciarServidor();
 
-console.log("MONGO:", process.env.MONGO_URI);
+console.log("Servidor iniciado");
